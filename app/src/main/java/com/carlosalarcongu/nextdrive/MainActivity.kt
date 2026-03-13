3dbafdb0625188ea97b4d019d1213f58d52464b8
Carlos Alarcoon <carlos.alarcoon@gmail.com>
2026-03-13 16:22:44 +0100

    Commit inicial: Estructura base de NextDrive

---
+++ app/src/main/java/com/carlosalarcongu/nextdrive/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.carlosalarcongu.nextdrive
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.carlosalarcongu.nextdrive.ui.theme.NextDriveTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NextDriveTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NextDriveTheme {
+        Greeting("Android")
+    }
+}
